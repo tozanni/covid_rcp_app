caaf582a0eb47058fee9f150230b10bcc4c68ea1
Enrique García <enrique@inodata.com.mx>
2020-07-09 05:50:04 -0500

    Actualiza nombre de campos en formularios

--- src/Form/LiverFunctionType.php
+++ src/Form/LiverFunctionType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\LiverFunction;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,9 @@ class LiverFunctionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('aspartateAminotransferase')
-            ->add('alanineTransaminase')
-            ->add('bloodUreaNitrogen')
+            ->add('aspartate_aminotransferase', TextType::class)
+            ->add('alanine_transaminase', TextType::class)
+            ->add('blood_urea_nitrogen', TextType::class)
             ->add('record')
         ;
     }
