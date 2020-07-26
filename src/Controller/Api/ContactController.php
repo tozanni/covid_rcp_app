251bba648bf9699e8b6f527bf38c99b8eceb2cd2
Enrique García <enrique@inodata.com.mx>
2020-07-26 02:19:57 -0500

    Agrega endpoint para el formulario de contacto

--- src/Controller/Api/ContactController.php
+++ src/Controller/Api/ContactController.php
@@ -6,8 +6,15 @@ use FOS\RestBundle\Controller\{AbstractFOSRestController, Annotations as Rest};
 use App\Entity\Contact;
 use App\Form\ContactType;
 use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Component\HttpFoundation\{Request, Response};
+use Swagger\Annotations as SWG;
 
+/**
+ * Class ContactController
+ * @package App\Controller\Api
+ * @SWG\Tag(name="Contact")
+ */
 class ContactController extends AbstractFOSRestController
 {
     use ProcessFormsTrait;
@@ -16,10 +23,20 @@ class ContactController extends AbstractFOSRestController
      * @Rest\Post("/contacts", name="contact")
      * @param Request $request
      * @return View
+     *
+     * @SWG\Parameter(name="body", in="body", @SWG\Schema(type="object",
+     *  @SWG\Property(property="name", type="string", description="El nombre del contacto", example="Jhon Doe"),
+     *  @SWG\Property(property="subject", type="string", description="Título del asunto, viene del dropdown", example="Quiero Colaborar"),
+     *  @SWG\Property(property="email", type="string", description="Correo electrónico del contacto", example="jhon@example.com"),
+     *  @SWG\Property(property="message", type="text", description="Mensaje del contacto", example="Parrafo con el mensaje de texto a enviar"),
+     * ))
+     *
+     * @SWG\Response(response=201, description="El mensaje ha sido creado y enviado", @Model(type=Contact::class))
      */
     public function create(Request $request): View
     {
         $contact = new Contact();
+
         $form = $this->createForm(ContactType::class, $contact);
         $this->processForm($request, $form);
 
