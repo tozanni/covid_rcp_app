afb5485878a99d4d8edccba9fbb77f05813f5475
Enrique García <enrique@inodata.com.mx>
2020-08-27 22:32:31 -0500

    Corrige indexes para entidades de loggable

--- src/Entity/Loggable/VitalSigns.php
+++ src/Entity/Loggable/VitalSigns.php
@@ -12,10 +12,10 @@ use Doctrine\ORM\Mapping as ORM;
  *     name="vital_signs_log_entries",
  *     options={"row_format":"DYNAMIC"},
  *  indexes={
- *      @ORM\Index(name="log_class_lookup_idx", columns={"object_class"}),
- *      @ORM\Index(name="log_date_lookup_idx", columns={"logged_at"}),
- *      @ORM\Index(name="log_user_lookup_idx", columns={"username"}),
- *      @ORM\Index(name="log_version_lookup_idx", columns={"object_id", "object_class", "version"})
+ *      @ORM\Index(name="vital_signs_log_class_lookup_idx", columns={"object_class"}),
+ *      @ORM\Index(name="vital_signs_log_date_lookup_idx", columns={"logged_at"}),
+ *      @ORM\Index(name="vital_signs_log_user_lookup_idx", columns={"username"}),
+ *      @ORM\Index(name="vital_signs_log_version_lookup_idx", columns={"object_id", "object_class", "version"})
  *  }
  * )
  * @ORM\Entity(repositoryClass="Gedmo\Loggable\Entity\Repository\LogEntryRepository")
