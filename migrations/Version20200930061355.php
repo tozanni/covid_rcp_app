19aba40e0511f05884a2e2f631dded4931a2b8c8
Enrique García <enrique@inodata.com.mx>
2020-09-30 01:28:26 -0500

    Corrige migración invertida de tablas sessions, revisions

--- migrations/Version20200930061355.php
+++ migrations/Version20200930061355.php
@@ -20,19 +20,18 @@ final class Version20200930061355 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE session_sess_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE revisions_id_seq CASCADE');
-        $this->addSql('DROP TABLE sessions');
-        $this->addSql('DROP TABLE revisions');
+        $this->addSql('CREATE SEQUENCE session_sess_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE revisions_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE sessions (sess_id VARCHAR(128) NOT NULL, sess_data BYTEA NOT NULL, sess_time INT NOT NULL, sess_lifetime INT NOT NULL, PRIMARY KEY(sess_id))');
+        $this->addSql('CREATE TABLE revisions (id SERIAL NOT NULL, "timestamp" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, username VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('CREATE SEQUENCE session_sess_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE revisions_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE sessions (sess_id VARCHAR(128) NOT NULL, sess_data BYTEA NOT NULL, sess_time INT NOT NULL, sess_lifetime INT NOT NULL, PRIMARY KEY(sess_id))');
-        $this->addSql('CREATE TABLE revisions (id SERIAL NOT NULL, "timestamp" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, username VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('DROP SEQUENCE session_sess_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE revisions_id_seq CASCADE');
+        $this->addSql('DROP TABLE sessions');
+        $this->addSql('DROP TABLE revisions');
     }
 }
